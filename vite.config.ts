7177c2b116189959e1e0746de80eaa9e9cacc7e8
ykagesg7 <52264421+ykagesg7@users.noreply.github.com>
2025-01-28 07:09:25 +0900

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -13,5 +13,6 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     }
   },
-  assetsInclude: ['**/*.geojson']
+  assetsInclude: ['**/*.geojson'],
+  base: '/Flight-planner/'
 });
